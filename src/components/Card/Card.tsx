1f07352d8971dd2fc2639c3dc84af7ab813c115a
Tom Charman <tom@thegreatescapism.com>
2022-02-23 23:06:26 +1100

    Nice animation

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -20,29 +20,28 @@ function Card({
 }: CardTypes) {
   const middle = <Markdown>{description}</Markdown>;
 
-  if (revealed) {
-    return (
-      <div
-        className={classNames(styles.card, isIt ? styles.good : styles.bad)}
-      >
-        {middle}
-      </div>
-    );
-  }
-
   const styleProperty : React.CSSProperties = usesImages
     ? { backgroundImage: `url('/images/${description.replace(/ |'/g, '_')}.jpg')` }
     : {};
 
+  const revealClass = isIt ? styles.good : styles.bad;
+
   return (
-    <button
-      type="button"
-      className={styles.card}
-      onClick={onSelect}
-      style={styleProperty}
-    >
-      {middle}
-    </button>
+    <div className={classNames(styles.flip, { [styles.revealed]: revealed })}>
+      <div className={styles.flipInner}>
+        <button
+          type="button"
+          className={styles.card}
+          onClick={onSelect}
+          style={styleProperty}
+        >
+          {middle}
+        </button>
+        <div className={classNames(styles.cardBack, revealClass)}>
+          {middle}
+        </div>
+      </div>
+    </div>
   );
 }
 
