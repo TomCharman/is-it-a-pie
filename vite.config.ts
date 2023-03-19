19d2e789f2a3b9d8623facdc9d6d62c26c5bde5c
Tom Charman <tom@thegreatescapism.com>
2023-03-19 21:50:53 +1100

    Vite!

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts'
+  }
+})
