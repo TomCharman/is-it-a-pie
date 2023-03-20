f58fe04127cc1b1f32f0982552039e3d3fa8c1b7
Tom Charman <tom@thegreatescapism.com>
2023-03-20 22:39:28 +1100

    Styling fixes

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -10,14 +10,14 @@ function Home() {
   const randomRound = Math.ceil(Math.random() * totalRounds);
 
   return (
-    <>
+    <div className={styles.base}>
       <Row>
         <Link className={clsx(styles.invite, 'col-md-6 offset-md-3')} to="/round/1">Start from the beginning</Link>
       </Row>
       <Row>
         <Link className={clsx(styles.invite, 'col-md-6 offset-md-3')} to={`/round/${randomRound}`}>I’m feeling lucky</Link>
       </Row>
-    </>
+    </div>
   );
 }
 
